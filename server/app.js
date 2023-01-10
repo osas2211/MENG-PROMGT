e0e17e8eeaea937fb39870427043d4f776b906cf
Osaretin Frank <osaretin.frank10@gmail.com>
2023-01-10 12:13:05 +0100

    added sample Data

--- server/app.js
+++ server/app.js
@@ -1,2 +1,16 @@
 require("dotenv").config()
 const express = require("express")
+const { graphqlHTTP } = require("express-graphql")
+const schema = require("./schema")
+const app = express()
+const PORT = process.env.PORT || 5000
+
+app.use(
+  "graphql",
+  graphqlHTTP({
+    schema,
+    graphiql: process.env.NODE_ENV === "development",
+  })
+)
+
+app.listen(PORT, () => console.log(`listening at PORT ${PORT}`))
